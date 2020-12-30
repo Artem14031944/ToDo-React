5f6f9c65fef1254dad61046947ce91ce2da0dfe8
Artem <Artem141221@gmail.com>
2020-12-30 16:31:58 +0300

    checKbox

---
+++ src/components/switch/darkStyle.jsx
@@ -0,0 +1,11 @@
+const darkStyles = `
+
+html {
+  background-color: #181A1B;
+}
+h1,h2,h3,h4,h5,h6 {
+  color: #fff;
+}
+
+`
+export default darkStyles
--- src/components/switch/layout.jsx
+++ src/components/switch/layout.jsx
@@ -18,6 +18,6 @@ export default props => {
   }
   
   return(
-   <>  {props.children} </>
+   <>{props.children} </>
   )
 }
